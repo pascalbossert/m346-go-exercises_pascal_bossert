63f8e8b763462fe6ce8c45a4bfbc25b48ac3a93a
Pascal Bossert <Pascal_Bossert@sluz.ch>
2024-11-25 11:05:23 +0100

    go-1-ex-3

--- go-1-ex-3/main.go
+++ go-1-ex-3/main.go
@@ -3,19 +3,22 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 )
 
 func main() {
-	var eyes = rand.Intn(5) + 1
+	rand.Seed(time.Now().UnixNano()) // Seed für Zufallszahlen
+	var eyes = rand.Intn(6) + 1
 	var when = time.Now()
 
-	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice shows", eyes, "eyes")
+	// Ausgabe der Augenzahl auf os.Stdout
+	fmt.Fprintln(os.Stdout, "The dice shows", eyes, "eyes")
 
-	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice was rolled at", when)
+	// Ausgabe der Zeit auf os.Stderr
+	fmt.Fprintln(os.Stderr, "The dice was rolled at", when)
 
-	// TODO: how to write the output into eyes.txt and dice.log?
-	// go run ex3/main.go TODO
+	// Hinweise für die Aufrufe:
+	// Um die Augenzahl in eyes.txt und die Zeit in dice.log zu speichern:
+	// go run ex3/main.go > eyes.txt 2> dice.log
 }
