794baa433b5dfd52c14c2de26500712731c80e40
Pascal Bossert <Pascal_Bossert@sluz.ch>
2024-11-25 11:37:39 +0100

    go-2-ex-3

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -3,14 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: create a map called "modules"
+	// Map mit Modulinformationen erstellen
+	modules := map[int]string{
+		104: "Programmieren Grundlagen",
+		117: "Datenbanken Modellierung",
+		346: "Cloud-Lösungen konzipieren und realisieren",
+	}
 
+	// Vorhandene Informationen ausgeben
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
-	// TODO: delete one
-	// TODO: add one
-	// TODO: replace one
+	// Änderungen an der Map vornehmen
+	delete(modules, 117)                             // Modul 117 entfernen
+	modules[500] = "Künstliche Intelligenz anwenden" // Neues Modul hinzufügen
+	modules[104] = "Programmieren fortgeschritten"   // Modul 104 ersetzen
+
+	// Map ausgeben
 	fmt.Println(modules)
 }
