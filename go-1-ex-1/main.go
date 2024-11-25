3a3fe90459c254c6b279966fa2c9dfc7c48400e7
Pascal Bossert <Pascal_Bossert@sluz.ch>
2024-11-25 10:45:57 +0100

    go-1-ex-1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: Declare and initialize the variables being used in the output!
+	// Deklaration und Initialisierung der Variablen
+	firstName := "Pascal"  // Vorname
+	lastName := "Bossert"  // Nachname
+	dayOfBirth := 16       // Geburtsdatum (Tag)
+	monthOfBirth := 9      // Geburtsdatum (Monat)
+	yearOfBirth := 1994    // Geburtsdatum (Jahr)
+	numberOfSiblings := 2  // Anzahl Geschwister
+	heightInMeters := 1.64 // Größe in Metern
+	zodiacSign := '\u264D' // Sternzeichen: Jungfrau (Unicode)
+
+	// Ausgabe der persönlichen Angaben
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
