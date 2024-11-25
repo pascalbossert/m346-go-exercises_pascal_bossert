08c65cd57d77598bec0bd0139bc7552f9a898182
Pascal Bossert <Pascal_Bossert@sluz.ch>
2024-11-25 13:11:44 +0100

    go-4-ex-3

--- go-4-ex-3/main.go
+++ go-4-ex-3/main.go
@@ -1,7 +1,30 @@
 package main
 
-// TODO: implement the function computeQuadraticFormula
+import (
+	"fmt"
+	"math"
+)
+
+// computeDiscriminant calculates the discriminant of the quadratic equation.
+func computeDiscriminant(a, b, c float64) float64 {
+	return math.Pow(b, 2) - 4*a*c
+}
+
+// computeQuadraticFormula solves the quadratic equation using the discriminant.
+func computeQuadraticFormula(a, b, c float64) []float64 {
+	d := computeDiscriminant(a, b, c)
+	if d > 0 {
+		root1 := (-b + math.Sqrt(d)) / (2 * a)
+		root2 := (-b - math.Sqrt(d)) / (2 * a)
+		return []float64{root1, root2}
+	} else if d == 0 {
+		return []float64{-b / (2 * a)}
+	}
+	return nil
+}
 
 func main() {
-	// TODO: call the function computeQuadraticFormula
+	fmt.Println(computeQuadraticFormula(3, 4, 1)) // Two solutions
+	fmt.Println(computeQuadraticFormula(2, 4, 2)) // One solution
+	fmt.Println(computeQuadraticFormula(3, 4, 2)) // No solution
 }
