9719e5bb7ed80aa9e3a3a92c28dd595def684ab4
Pascal Bossert <Pascal_Bossert@sluz.ch>
2024-11-25 11:38:28 +0100

    go-2-ex-4

--- go-2-ex-4/main.go
+++ go-2-ex-4/main.go
@@ -1,8 +1,55 @@
 package main
 
+import "fmt"
+
+// Strukturen definieren
+type Student struct {
+	FirstName string
+	LastName  string
+}
+
+type Class struct {
+	Students []Student
+}
+
+type Module struct {
+	Number  int
+	Name    string
+	Classes []Class
+}
+
 func main() {
-	// TODO: declare a type for Student (with first and last name)
-	// TODO: declare a type for Class (consisting of multiple students)
-	// TODO: declare a map of modules being attended by multiple classes
-	// TODO: output everything using fmt.Println()
+	// Beispieldaten erstellen
+	class1 := Class{
+		Students: []Student{
+			{FirstName: "Alice", LastName: "Müller"},
+			{FirstName: "Bob", LastName: "Schmidt"},
+			{FirstName: "Charlie", LastName: "Meier"},
+		},
+	}
+
+	class2 := Class{
+		Students: []Student{
+			{FirstName: "Diana", LastName: "Keller"},
+			{FirstName: "Eve", LastName: "Bach"},
+			{FirstName: "Frank", LastName: "Weber"},
+		},
+	}
+
+	modules := []Module{
+		{Number: 104, Name: "Programmieren Grundlagen", Classes: []Class{class1, class2}},
+		{Number: 117, Name: "Datenbanken Modellierung", Classes: []Class{class1}},
+		{Number: 346, Name: "Cloud-Lösungen konzipieren und realisieren", Classes: []Class{class2}},
+	}
+
+	// Daten ausgeben
+	for _, module := range modules {
+		fmt.Printf("Modul %d: %s\n", module.Number, module.Name)
+		for i, class := range module.Classes {
+			fmt.Printf("  Klasse %d:\n", i+1)
+			for _, student := range class.Students {
+				fmt.Printf("    - %s %s\n", student.FirstName, student.LastName)
+			}
+		}
+	}
 }
