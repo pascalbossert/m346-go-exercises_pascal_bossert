9924339efe74a911e06043e078c417ebb919c08f
Pascal Bossert <Pascal_Bossert@sluz.ch>
2024-11-25 13:12:15 +0100

    go-4-ex-4

--- go-4-ex-4/main.go
+++ go-4-ex-4/main.go
@@ -1,9 +1,34 @@
 package main
 
-// TODO: implement the function convertCelsiusToFahrenheit
-// TODO: implement the function convertFahrenheitToCelsius
+import "fmt"
+
+// Conversion functions
+func convertCelsiusToFahrenheit(celsius float64) float64 {
+	return celsius*9/5 + 32
+}
+
+func convertFahrenheitToCelsius(fahrenheit float64) float64 {
+	return (fahrenheit - 32) * 5 / 9
+}
+
+// Types for methods
+type Celsius float64
+type Fahrenheit float64
+
+func (c Celsius) ConvertToFahrenheit() Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
+
+func (f Fahrenheit) ConvertToCelsius() Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
 
 func main() {
-	// TODO: call the function convertCelsiusToFahrenheit
-	// TODO: call the function convertFahrenheitToCelsius
+	fmt.Println(convertCelsiusToFahrenheit(23))   // 73.4
+	fmt.Println(convertFahrenheitToCelsius(73.4)) // ~23.0
+
+	var tempC Celsius = 23
+	var tempF Fahrenheit = tempC.ConvertToFahrenheit()
+	fmt.Println(tempF)                    // 73.4
+	fmt.Println(tempF.ConvertToCelsius()) // ~23.0
 }
