478d94a8343e0852a84806253904496046c31f5f
Pascal Bossert <Pascal_Bossert@sluz.ch>
2024-11-25 13:11:20 +0100

    go-4-ex-2

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,26 @@
 package main
 
-// TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+import (
+	"fmt"
+	"math"
+)
+
+// computeHypotenuse calculates the hypotenuse of a triangle.
+func computeHypotenuse(a, b float64) float64 {
+	return math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2))
+}
+
+type ShortSides struct {
+	a, b float64
+}
+
+// Hypotenuse calculates the hypotenuse based on the struct.
+func (s ShortSides) Hypotenuse() float64 {
+	return computeHypotenuse(s.a, s.b)
+}
 
 func main() {
-	// TODO: call the function computeHypotenuse
+	fmt.Println(computeHypotenuse(3, 4))           // 5.000
+	fmt.Println(ShortSides{6, 8}.Hypotenuse())     // 10.000
+	fmt.Println(ShortSides{1.5, 2.0}.Hypotenuse()) // ~2.500
 }
